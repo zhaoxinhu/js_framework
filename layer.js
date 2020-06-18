510beedc822f39569ea5b22936c84c11cc8d6587
zhaoxinhu <759054522@qq.com>
2020-06-18 12:29:28 +0800

    取消ES6写法

--- layer.js
+++ layer.js
@@ -59,15 +59,14 @@
     // 遮罩
     function _showShade() {
         var shadeDiv = document.createElement("div");
-        shadeDiv.style.cssText=`
-            width:100%;
-            height:100%;
-            background-color:rgba(0, 0, 0,0.3);
-            zIndex:19891017;
-            position:fixed;
-            top:0;
-            left:0;
-        `
+        shadeDiv.style.cssText=
+            "width:100%;" + 
+            "height:100%;" + 
+            "background-color:rgba(0, 0, 0,0.3);" + 
+            "zIndex:19891017;" + 
+            "position:fixed;" + 
+            "top:0;" + 
+            "left:0;";
         shadeDiv.id = "layer-open-shade";
         document.body.append(shadeDiv);
     }
@@ -75,15 +74,14 @@
     function _showOpenDiv(title, content, options) {
         // 1、 构造大的div
         var openDiv = document.createElement("div");
-        openDiv.style.cssText=`
-            width:260px;
-            height:154px;
-            position:fixed;
-            zIndex:19891018;
-            border-radius:2px;
-            background-color:#fff;
-            box-shadow:1px 1px 50px rgba(0,0,0,.3);
-        `
+        openDiv.style.cssText=
+            "width:260px;" + 
+            "height:154px;" + 
+            "position:fixed;" + 
+            "zIndex:19891018;" + 
+            "border-radius:2px;" + 
+            "background-color:#fff;" + 
+            "box-shadow:1px 1px 50px rgba(0,0,0,.3);";
         openDiv.id = "layer-open-div";
         // 居中显示
         //  获取可视化区域大小，计算显示位置
@@ -93,45 +91,42 @@
         openDiv.style.top = ((clientHeight - 154) / 2) + "px";
 
         //监听window大小变化
-        window.addEventListener('resize',(res)=>{
+        window.addEventListener('resize', function(res){
             openDiv.style.left = ((res.target.innerWidth - 260) / 2) + "px";
             openDiv.style.top = ((res.target.innerHeight - 154) / 2) + "px";
-        })
+        });
 
         // 2、 构造title
         var titleDiv = document.createElement("div");
         titleDiv.innerText = title?title:'在线调试';
-        titleDiv.style.cssText=`
-            height:42px;
-            border-bottom:1px solid #eee;
-            padding:0 80px 0 20px;
-            line-height:42px;
-            font-size:14px;
-            color:#333;
-            overflow:hidden;
-            background-color:#F8F8F8;
-            border-radius:2px 2px 0 0;
-        `
+        titleDiv.style.cssText=
+            "height:42px;" + 
+            "border-bottom:1px solid #eee;" + 
+            "padding:0 80px 0 20px;" + 
+            "line-height:42px;" + 
+            "font-size:14px;" + 
+            "color:#333;" + 
+            "overflow:hidden;" + 
+            "background-color:#F8F8F8;" + 
+            "border-radius:2px 2px 0 0;";
 
         // 3、 构造content
         var contentDiv = document.createElement("div");
         contentDiv.innerText = content?content:'显示内容';
-        contentDiv.style.cssText=`
-            position:relative;
-            padding:20px;
-            line-hight:24px;
-            word-break:break-all;
-            font-size:14px;
-            overflowX:hidden;
-            overflowY:auto;
-        `
+        contentDiv.style.cssText=
+            "position:relative;" + 
+            "padding:20px;" + 
+            "line-hight:24px;" + 
+            "word-break:break-all;" + 
+            "font-size:14px;" + 
+            "overflowX:hidden;" + 
+            "overflowY:auto;";
         // 4、 构造btn
         var btnDiv = document.createElement("div");
-        btnDiv.style.cssText=`
-            text-align:right;
-            padding:0 15px 12px;
-            pointer-events:auto;
-        `
+        btnDiv.style.cssText=
+            "text-align:right;" + 
+            "padding:0 15px 12px;" + 
+            "pointer-events:auto;";
 
         var btn1 = _createBtn("确定");
         btn1.id = "layer-open-btn-yes";
@@ -160,21 +155,19 @@
     // 创建按钮
     function _createBtn(content) {
         var btn1 = document.createElement("a");
-        btn1.style.cssText=`
-            height:28px;
-            line-height:28px;
-            margin:5px 5px 0;
-            padding:0 15px;
-            border:1px solid #1E9FFF;
-            border-radius:2px;
-            font-weight:400;
-            cursor:pointer;
-            text-decoration:none;
-            background-color:#1E9FFF;
-            color:#fff;
-            display:inline-block;
-
-        `
+        btn1.style.cssText=
+            "height:28px;" + 
+            "line-height:28px;" + 
+            "margin:5px 5px 0;" + 
+            "padding:0 15px;" + 
+            "border:1px solid #1E9FFF;" + 
+            "border-radius:2px;" + 
+            "font-weight:400;" + 
+            "cursor:pointer;" + 
+            "text-decoration:none;" + 
+            "background-color:#1E9FFF;" + 
+            "color:#fff;" + 
+            "display:inline-block;";
         btn1.innerText = content;
         return btn1;
     }
