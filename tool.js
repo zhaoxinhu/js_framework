e9228864872d297ac2f8eec08f9a5d82eaeeb8ab
zhaoxinhu <759054522@qq.com>
2020-12-25 16:04:25 +0800

    调整tool.js调用方式

--- tool.js
+++ tool.js
@@ -1,87 +1,31 @@
 (function(window) {
-    var TOOL = window.TOOL;
-    if (!TOOL) {
-        TOOL = {};
-    }
-
-    TOOL["ajax"] = {};
-    /**
-     * 异步get 请求
-     * @param url
-     * @param handler
-     * @param errorHandler
-     */
-    TOOL.ajax.get = function(url, handler, errorHandler) {
-        _sendRequest(url, true, null, handler, errorHandler, "get");
-    };
-
-    TOOL.ajax.get = function(options) {
-        _sendRequest(options.url, true, null, options.success, options.error, "get");
-    };
-
-    /**
-     * 同步get 请求
-     * @param url
-     * @param handler
-     * @param errorHandler
-     */
-    TOOL.ajax.asyncGet = function(url, handler, errorHandler) {
-        _sendRequest(url, false, null, handler, errorHandler, "get");
-    };
-
-    /**
-     * 异步 post请求，返回json
-     * @param url               请求地址
-     * @param handler           成功处理函数
-     * @param errorHandler      失败处理函数
-     */
-    TOOL.ajax.postNoData = function (url, handler, errorHandler) {
-        _ajax_format(url, true, null, handler, errorHandler);
-    };
-    /**
-     * 异步 post请求，返回json
-     * @param url               请求地址
-     * @param data              请求数据
-     * @param handler           成功处理函数
-     * @param errorHandler      失败处理函数
-     */
-    TOOL.ajax.post = function (url, data, handler, errorHandler) {
-        _ajax_format(url, true, data, handler, errorHandler);
-    };
-    /**
-     * 同步 post请求，返回json
-     * @param url               请求地址
-     * @param data              请求数据
-     * @param handler           成功处理函数
-     * @param errorHandler      失败处理函数
-     */
-    TOOL.ajax.asyncPost = function (url, data, handler, errorHandler) {
-        _ajax_format(url, false, data, handler, errorHandler);
-    };
-
-    /**
-     * 封装ajax请求
-     * @param url
-     * @param async                 true为异步，false为同步
-     * @param data
-     * @param successHandler
-     * @param errHandler
-     * @private
-     */
-    function _ajax_format(url, async, data, successHandler, errHandler) {
-        var postUrl = url,
-            postData;
-        if (typeof data == 'object') {
-            postData = data;
-        } else if (typeof data == 'function') {
-            successHandler = data;
-            if (successHandler) {
-                errHandler = successHandler;
+    let TOOL = {
+        ajax: {
+            get: function(url, success, err) {
+                _sendRequest(url, true, null, success, error, "get");
+            },
+            syncGet: function(url, success, err) {
+                _sendRequest(url, false, null, success, error, "get");
+            },
+            post: function(url, data, success, err) {
+                _sendRequest(url, true, data, success, error, "post");
+            },
+            postNoData: function(url, success, err) {
+                _sendRequest(url, true, null, success, error, "post");
+            },
+            syncPost: function(url, data, success, err) {
+                 _sendRequest(url, false, data, success, error, "post");
+            }
+        },
+        validator: {
+            isMobile: function(mobile) {
+                return _is_mobile(mobile);
             }
         }
-        _sendRequest(postUrl, async, postData, successHandler, errHandler, "post");
-    }
+    };
 
+    window.TOOL = TOOL;
+    
     /**
      * 发送真正的http请求
      * @param url
@@ -116,12 +60,7 @@
     	let mobileReg = /^(((13[0-9]{1})|159|153)+\d{8})$/;
     	return mobileReg.test(mobile);
     }
-    let Validator = {
-    	isMobile: _is_mobile
-    };
-    TOOL.Validator = Validator;
 
-    
-    window.TOOL = TOOL;
+
 
 })(window);
